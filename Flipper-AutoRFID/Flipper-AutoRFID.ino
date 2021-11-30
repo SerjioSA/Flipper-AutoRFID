5ad58a4584c78fe600d0b1859539f4a37d7f3710
SerjioSA <37222543+SerjioSA@users.noreply.github.com>
2021-11-30 19:56:22 +0300

    Adding bluetooth test
    Test

--- Flipper-AutoRFID/Flipper-AutoRFID.ino
+++ Flipper-AutoRFID/Flipper-AutoRFID.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
-
+int val;
+int LED = 13;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, HIGH);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  if (Serial.available())
+  {
+    val = Serial.read();
+    // При символе "1" включаем светодиод
+    if (val == '1')
+    {
+      digitalWrite(LED, HIGH);
+    }
+    // При символе "0" выключаем светодиод
+    if ( val == '0')
+    {
+      digitalWrite(LED, LOW);
+    }
+  }
 }
